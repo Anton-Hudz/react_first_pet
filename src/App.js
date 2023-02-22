b55d1a2d8890d24535b7980954ed96bb5a9fedfc
Anton <pat280885@mail.ru>
2023-02-22 15:28:55 +0200

    feat:first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Technologies from './components/Technologies/Technologies';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Technologies />
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Technologies/Technologies.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "./SideBar/Sidebar";
+
+const Technologies = () => {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Technologies;
